8c21825984be4e88751d4c6975faa8526d2cd28d
Jacek Sieka <arnetheduck@gmail.com>
2024-01-08 11:31:21 +0100

    better format script

--- format-git-repo.sh
+++ format-git-repo.sh
@@ -3,10 +3,20 @@
 
 set -eo pipefail
 
+git diff --exit-code > /dev/null || { echo "Commit changes before doing a (potentially) large reformat!" ; exit 1 ; }
+
 git ls-files | grep ".nim$" | xargs -n1 nph
+
+! git diff --exit-code > /dev/null || { echo "This repository is already formatted" ; exit 0 ; }
 
 git commit -am "Formatted with nph $(nph --version)"
 
-echo "Add the following two lines to .git-blame-ignore-revs to maintain git blame:"
-echo "# Formatted with nph $(nph --version)"
-echo "$(git rev-parse HEAD)"
+echo "# Formatted with nph $(nph --version)" >> .git-blame-ignore-revs
+echo "$(git rev-parse HEAD)" >> .git-blame-ignore-revs
+
+git commit -am "Add $(git rev-parse HEAD) to .git-blame-ignore-revs"
+
+echo "The repo has been updated with two commits recording the reformatting and blame information."
+echo "You can review the changes with 'git diff HEAD^^' before pushing to a public repository."
+echo "If you don't want blame information, remove the last commit with 'git reset --hard HEAD^'."
+
